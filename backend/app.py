43185ba8c882d789f0d12f04e840ef88e4c340fa
srikarthickjc2024ece-ui <srikarthick.jc2024ece@sece.ac.in>
2026-01-03 17:44:33 +0530

    Render fix

--- backend/app.py
+++ backend/app.py
@@ -109,4 +109,5 @@ def status():
 # Run Server (NO debug mode)
 # --------------------------------
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
